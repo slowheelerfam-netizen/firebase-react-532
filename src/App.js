b141fe0821f19b63d9763b48acd9d246149338d4
donwheeler <slowheelerfam@gmail.com>
2026-01-12 16:30:48 -0800

    Finished with firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { auth } from './firebase/init';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut, 
+  onAuthStateChanged
+ } from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => { 
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      if (user) {
+        setUser(user);        
+      }
+    });
+  }, []);
   function register() {
       createUserWithEmailAndPassword(auth, 'email@example.com', 'password123')
         .then((user) => {
@@ -15,19 +32,28 @@ function App() {
   }
 
   function login() {
-    createUserWithEmailAndPassword(auth, 'email@example.com', 'password123')
-        .then((user) => {
-          console.log(user)
+    signInWithEmailAndPassword(auth, 'email@example.com', 'password123')
+        .then(({ user }) => {
+            console.log(user)
+          setUser(user)
         })
         .catch((error) => {
-          console.log('error')
+          console.log(error);
         })
   }
+
+    function logout() {
+      signOut(auth);
+      setUser({});
+        }
+    
   
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'Loading...' : user.email}
     </div>
   );
 }
